b47c401ae7c544da90957a254fa4b64e5b90b76a
Binod Chaudhary <thebinod7@gmail.com>
2024-11-10 09:15:28 +0545

    css styles config and setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,57 @@
 import React from "react";
+import "../styles/globals.css";
 
-const _app = () => {
-  return <div>_app</div>;
-};
+import merge from "lodash/merge";
+import "@rainbow-me/rainbowkit/styles.css";
 
-export default _app;
+import {
+    getDefaultWallets,
+    RainbowKitProvider,
+    darkTheme,
+    midnightTheme,
+} from "@rainbow-me/rainbowkit";
+
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { infuraProvider } from "wagmi/providers/infura";
+
+const INFURA_API_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY;
+
+const { chains, provider } = configureChains(
+    [chain.polygonMumbai],
+    [
+        infuraProvider({
+            apiKey: INFURA_API_KEY,
+            priority: 1,
+        }),
+    ]
+);
+
+const { connectors } = getDefaultWallets({
+    appName: "Dex",
+    chains,
+});
+
+const wagmiClient = createClient({
+    autoConnect: true,
+    provider,
+    connectors,
+});
+
+const myTheme = merge(midnightTheme(), {
+    colors: {
+        accentColor: "#18181b",
+        accentColorForeground: "#fff",
+    },
+});
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <WagmiConfig client={wagmiClient}>
+            <RainbowKitProvider chains={chains} theme={myTheme}>
+                <Component {...pageProps} />
+            </RainbowKitProvider>
+        </WagmiConfig>
+    );
+}
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Header, Footer, HeroSection, Card } from "../components";
 
-const index = () => {
-    return <div>index</div>;
+const home = () => {
+    return (
+        <div className="bg-[#1A1A1A]">
+            <Header />
+            <HeroSection />
+            <Card />
+            <Footer />
+        </div>
+    );
 };
 
-export default index;
+export default home;
